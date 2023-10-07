bb396004ac91c60d4a58866ed9d99565f191d788
kudawoo2002 <kudawoo2002@gmail.com>
2023-10-07 08:06:51 +0000

    Final code
    reset button and auto aply coming soon

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const btnRock = document.querySelector(".js-rock-button");
+const btnPaper = document.querySelector(".js-paper-button");
+const btnScissors = document.querySelector(".js-scissors-button");
+const yourMove = document.querySelector(".your-moves");
+const computerMove = document.querySelector(".js-moves");
+const resultMove = document.querySelector(".result");
+const playerScore = document.querySelector(".player-score");
+const compScore = document.querySelector(".computer-score");
+const options = ["Rock", "Paper", "Scissors"];
+
+const randomNumber = () => {
+  return Math.trunc(Math.random() * options.length);
+};
+
+let playerPoint = 0;
+
+let compPoint = 0;
+
+btnRock.addEventListener("click", () => {
+  const userMove = "Rock";
+  yourMove.textContent = userMove;
+  const compMove = options[randomNumber()];
+  computerMove.textContent = compMove;
+  if (compMove === "Rock") {
+    resultMove.textContent = "It a tie";
+  } else if (compMove === "Paper") {
+    resultMove.textContent = "You lose!!!";
+    compPoint += 1;
+    compScore.textContent = compPoint;
+  } else if (compMove === "Scissors") {
+    resultMove.textContent = "You win!!!";
+    playerPoint += 1;
+    playerScore.textContent = playerPoint;
+  }
+});
+
+btnPaper.addEventListener("click", () => {
+  yourMove.textContent = "Paper";
+  const compMove = options[randomNumber()];
+  computerMove.textContent = compMove;
+  if (compMove === "Paper") {
+    resultMove.textContent = "It a tie";
+  } else if (compMove === "Rock") {
+    resultMove.textContent = "You win!!!";
+    playerPoint += 1;
+    playerScore.textContent = playerPoint;
+  } else if (compMove === "Scissors") {
+    resultMove.textContent = "You lose!!!";
+    compPoint += 1;
+    compScore.textContent = compPoint;
+  }
+});
+
+btnScissors.addEventListener("click", () => {
+  yourMove.textContent = "Scissors";
+  const compMove = options[randomNumber()];
+  computerMove.textContent = compMove;
+  if (compMove === "Paper") {
+    resultMove.textContent = "You win!!!";
+    playerPoint += 1;
+    playerScore.textContent = playerPoint;
+  } else if (compMove === "Rock") {
+    resultMove.textContent = "You lose!!!";
+    compPoint += 1;
+    compScore.textContent = compPoint;
+  } else if (compMove === "Scissors") {
+    resultMove.textContent = "It a tie";
+  }
+});
